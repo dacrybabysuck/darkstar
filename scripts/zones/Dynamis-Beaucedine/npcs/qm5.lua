76897095fb3444caf17a245f1c7cff0e5dc0da8b
dacrybabysuck <dacrybabysuck@nondescriptdomain.com>
2015-07-12 22:01:39 -0400

    added simple trades for relic tier 4 fights

---
+++ scripts/zones/Dynamis-Beaucedine/npcs/qm5.lua
@@ -0,0 +1,50 @@
+-----------------------------------
+-- Area: Dynamis Beaucidine
+-- NPC:  ??? (qm5)
+-- Involved Relic Bow
+-- @pos 99 -20 132 134
+-----------------------------------
+package.loaded["scripts/zones/Ordelles_Caves/TextIDs"] = nil;
+-----------------------------------
+
+require("scripts/globals/settings");
+require("scripts/zones/Ordelles_Caves/TextIDs");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+
+		if(trade:hasItemQty(3359,1) and trade:getItemCount() == 1) then -- Trade Deluder's Fortune Parchment
+			player:tradeComplete();
+			SpawnMob(17326086,180):updateClaim(player);
+		end
+
+end;
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+	player:messageSpecial(NOTHING_OUT_OF_ORDINARY);
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
